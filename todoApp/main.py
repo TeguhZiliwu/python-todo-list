45a02ed74ca281325ae696b230156c5c2c425e77
Teguh Ziliwu <teguh.ziliwu@gmail.com>
2025-02-23 10:50:06 +0700

    add bitwise operator

--- todoApp/main.py
+++ todoApp/main.py
@@ -4,16 +4,19 @@ todos = []
 
 while True:
     user_action = input(userPrompt)
+    user_action = user_action.strip()
 
     match user_action.lower():
         case "add":
             todo = input(todoPrompt)
             todos.append(todo.title())
-        case "show":
+        case "show" | "display":
             print("List of todo :")
             for item in todos:
                 print(item)
         case "exit":
             break
+        case randomCommand:
+            print("Hey, you entered an unknown command!")
 
 print("Program exit!")
